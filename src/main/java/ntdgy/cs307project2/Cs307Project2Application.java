1a1e9f72bf6c6adaec07ddcf27ad7c01f784df07
ntdgy <ntdgy2001@gmail.com>
2022-04-27 17:56:54 +0800

    init commit for fxxk CS307

---
+++ src/main/java/ntdgy/cs307project2/Cs307Project2Application.java
@@ -0,0 +1,13 @@
+package ntdgy.cs307project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs307Project2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs307Project2Application.class, args);
+	}
+
+}
