8df1bd300c81202734c1c2bb13ce702a2bcf862e
anshangPro <65436468+anshangPro@users.noreply.github.com>
2022-05-01 01:30:29 +0800

    update template

--- src/main/java/ntdgy/cs307project2/controller/DatabaseController.java
+++ src/main/java/ntdgy/cs307project2/controller/DatabaseController.java
@@ -1,21 +1,83 @@
 package ntdgy.cs307project2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("database")
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Controller
+@RequestMapping("/api/database")
 public class DatabaseController {
 
-    @PostMapping("/test")
-    public String test(){
-        return "test";
+    final DataSource dataSource;
+
+    public DatabaseController(DataSource dataSource){
+        this.dataSource = dataSource;
     }
 
-    @GetMapping("/test")
-    public String wa(){
-        return "WA method";
+    @PostMapping("/enterprise/add")
+    public String addEnterprise(
+            @RequestParam("id") int id,
+            @RequestParam("name") String name,
+            @RequestParam("country") String country,
+            @RequestParam("city") String city,
+            @RequestParam("supply_center") String supplyCenter,
+            @RequestParam("industry") String industry
+    ) throws SQLException {
+        var con = dataSource.getConnection();
+        var stmt = con.prepareStatement("insert into enterprise(id, name, supply_center_id, country, city, industry)"
+                + " values(?, ?, ?, ?, ?, ?)");
+        //stmt.setInt();
+        return "";
     }
+
+    @PostMapping("/enterprise/select")
+    public String selectEnterprise(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("country") String country,
+            @RequestParam("city") String city,
+            @RequestParam("supply_center") String supplyCenter,
+            @RequestParam("industry") String industry
+    ){
+        return "";
+    }
+
+    @PostMapping("/enterprise/delete")
+    public String deleteEnterprise(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("country") String country,
+            @RequestParam("city") String city,
+            @RequestParam("supply_center") String supplyCenter,
+            @RequestParam("industry") String industry
+    ){
+        return "";
+    }
+
+    @PostMapping("/enterprise/update")
+    public String updateEnterprise(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("country") String country,
+            @RequestParam("city") String city,
+            @RequestParam("supply_center") String supplyCenter,
+            @RequestParam("industry") String industry
+    ){
+        return "";
+    }
+
+    @PostMapping("/stockIn")
+    public String stockIn(
+            @RequestParam("supply_center") String supplyCenter,
+            @RequestParam("product_model") String productModel,
+            @RequestParam("supply_staff") String supplyStaff,
+            @RequestParam("date") String date,
+            @RequestParam("purchase_price") String price,
+            @RequestParam("quantity") String quantity
+    ){
+        return "";
+    }
+
 }
