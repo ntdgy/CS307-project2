9132529bdca18de94e0e31c6dd1c062ed9d272e5
anshangPro <65436468+anshangPro@users.noreply.github.com>
2022-05-18 18:46:56 +0800

    finish alert of stockin

--- src/main/resources/static/js/stockIn.js
+++ src/main/resources/static/js/stockIn.js
@@ -1,6 +1,6 @@
 var app = angular.module('stock', []);
 var table;
-app.controller('stockIn', function ($scope, $http) {
+app.controller('stockIn', function ($scope, $http, $timeout) {
     $scope.mysubmit = function () {
         console.log($scope.order)
         console.log($scope.order.date)
@@ -10,10 +10,15 @@ app.controller('stockIn', function ($scope, $http) {
             data: $scope.order
         }).then(
             function (data, status, header, config) {
-                $scope.return = data.data;
+                $scope.return = 'Success';
             }
         ).catch(function (data, status, header, config) {
             console.log(data, status);
+            $scope.returnFail = data.data.result;
         });
+        $timeout(function (){
+            $scope.return = '';
+            $scope.returnFail = '';
+        }, 3000);
     }
 })
